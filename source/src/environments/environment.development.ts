35cb7d8a715b6b21a174b75ae8998c48403aada7
GuillermoSH <siciliahernandezguillermo@gmail.com>
2025-04-07 13:04:33 +0100

    :sparkles: Changed the animation of the player cards; closes #79

--- source/src/environments/environment.development.ts
+++ source/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
 
-    apiUrl: "http://localhost:9097/api/v1",
+    //apiUrl: "http://localhost:9097/api/v1",
     imagesEndpoint: "http://localhost:9097/images/",
-    // apiUrl: "https://995dnb1x-9097.uks1.devtunnels.ms/api/v1",
+    apiUrl: "https://jwqqt330-9097.uks1.devtunnels.ms/api/v1",
     // imagesEndpoint: "https://995dnb1x-9097.uks1.devtunnels.ms/images/",
 };
